199fb3d3af88da1c128d6b2ccc56394e0543cc35
Jagannathan Mahalingam <dbob4079@gmail.com>
2023-10-28 13:49:31 +0530

    added alarm

--- alarm.py
+++ alarm.py
@@ -1,8 +1,23 @@
 import beepy
 import datetime
+import time
+from threading import Thread
 
-def alarm():
+def set_alarm():
     current = datetime.datetime.now()
     min = int(input("Enter time in minutes from now where alarm will activate"))
     time_change = datetime.timedelta(minutes=min)
-    new_time = current + time_change 
+    new_time = current + time_change
+    return new_time.strftime('%H:%M')
+
+def alarm():
+    t = set_alarm()
+    while True:
+        if  t == datetime.datetime.now().strftime('%H:%M'):
+            while True:
+                beepy.beep(sound=1)
+                time.sleep(0.25)
+
+# thread = Thread(target=alarm, daemon=True, name="alarm")
+# thread.start()
+
