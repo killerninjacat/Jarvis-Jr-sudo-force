a6bbda80c07a42c1f1b226ea8151b48fc7fce217
akshay <akshay.iit6@gmail.com>
2023-10-28 13:51:55 +0530

    feat(tts): added tts feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import click
 import requests
+import json
 import openai
 import os
 import tictactoeai
@@ -40,83 +41,88 @@ def get_default_terminal():
 
 
 default_terminal = get_default_terminal()
-openai.api_key="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"
+openai.api_key = "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"
+
 
 def hello(name):
     click.echo("Hello "+name)
 
 
 def chat_with_gpt(prompt):
-    # response = openai.ChatCompletion.create(
-    #     model="gpt-3.5-turbo",
-    #     messages=[{"role":"user", "content":f"reply only with the {default_terminal} command to do the following\n\n"+prompt}]
-    # )
-    # return response.choices[0].message.content.strip()
-    return "file deleted successfully"
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": f"{prompt}"}]
+    )
+    return response.choices[0].message.content.strip()
+    # return "file deleted successfully"
     # return "fuck you"
 
 
 def process_command(command):
-    if ("tictactoe" in command) or ("tic tac toe") in command:
-        os.system("clear")
-        tictactoeai.init()
-        return
-        
-    if (command in ["toss", "flip"]) and ("coin" in command):  #check
-        os.system("clear")
-        functions.tossCoin()
-        return
+    if not isinstance(command, dict):
+        if ("tictactoe" in command) or ("tic tac toe") in command:
+            os.system("clear")
+            tictactoeai.init()
+            return
 
-    if ("date" in command) or ("time" in command):
-        functions.dateAndTime(command)
-        return
+        if (command in ["toss", "flip"]) and ("coin" in command):  # check
+            os.system("clear")
+            functions.tossCoin()
+            return
 
-    if (command in ["stopwatch", "stop watch"] ):
-        functions.stopwatch()
-        return
+        if ("date" in command) or ("time" in command):
+            functions.dateAndTime(command)
+            return
 
-    if("weather" in command):
-        output = requests.get("https://www.wttr.in")
-        city=""
-        if("not found" in str(output.content)):
-            print("Sorry, not sure of your current city")
-            city = input("Enter your current city: ")
-        os.system("curl wttr.in/"+city)
-        return
+        if (command in ["stopwatch", "stop watch"]):
+            functions.stopwatch()
+            return
 
-    if ("countdown" in command) or ("timer" in command):
-        time = chat_with_gpt(f"below given sentence is a user generated sentence. find how long the timer should run, convert it to seconds and reply with only that message\n\n"+command)
-        # k = command.split()
-        # s =  int(k[-2])
-        functions.countdown(time)
-        return
-    
-    cmd = chat_with_gpt(f"extract the default terminal name from {default_terminal} reply only with the {default_terminal} command to do the following\n\n"+command)
+        if ("weather" in command):
+            output = requests.get("https://www.wttr.in")
+            city = ""
+            if ("not found" in str(output.content)):
+                print("Sorry, not sure of your current city")
+                city = input("Enter your current city: ")
+            os.system("curl wttr.in/"+city)
+            say("Showing weather results.. ohhhhhhhhhhh its coold")
+
+            return
+
+        if ("countdown" in command) or ("timer" in command):
+            time = chat_with_gpt(
+                f"below given sentence is a user generated sentence. find how long the timer should run, convert it to seconds and reply with only that message\n\n"+command)
+            # k = command.split()
+            # s =  int(k[-2])
+            functions.countdown(time)
+            return
+
+    cmd = chat_with_gpt(
+        f"provide the {default_terminal} command and a very short summary of the action (dont include this in command and dont include 'command:' just the command for the first is enough and give the short summary in past tense). return all this as a object datatypefor the following\n\n"+command)
     folderName = "folder_name"
     fileName = "file_name"
     if ("folder_name" in cmd):
         folderName = input("Enter a folder name ")
     if ("file_name" in cmd):
         fileName = input("Enter a file name ")
-<<<<<<< HEAD
         cmd.replace("file_name", fileName)
-    elif("file" in cmd):
-        fileName = input("Enter a file name ")
-        cmd.replace("file", fileName)
-    cmd = cmd.replace("folder_name", folderName)
-    res = os.system(cmd)
-    say("Action performed successfully")
+    # elif ("file" in cmd):
+    #     fileName = input("Enter a file name ")
+    #     cmd.replace("file", fileName)
+    print(type(cmd))
+    print(cmd)
+    if not (isinstance(cmd, dict)):
+        cmd = json.loads(cmd)
+    print(cmd["summary"])
 
-    print(cmd , res)
-=======
+    # cmd = cmd["command"].replace("folder_name", folderName)
+    res = os.system(cmd["command"])
+    if (cmd["summary"]):
+        say(cmd["summary"])
 
-    cmd = cmd.replace("folder_name", folderName).replace("file_name", fileName)
-    res = os.system(cmd)
-    say("Action performed successfully")
+    # print(cmd, res)
 
-    print(cmd, res)
-
->>>>>>> 0751544 (fix added few feat)
 
 while True:
     user_input = session.prompt("Enter your command in plain English: ")
